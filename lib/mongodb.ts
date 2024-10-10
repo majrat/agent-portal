140961eddcaf9b78a198cd44e0cd1a603b4cfcaf
majrat <mjjauharali@gmail.com>
2024-10-10 11:28:40 +0400

    added AZURE_db

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-const { MONGODB_URI } = process.env;
+const { MONGODB_URI, AZURE_COSMOS_CONNECTIONSTRING } = process.env;
 const options = { dbName: "priority-worldwide-bpa-db" };
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(
-      MONGODB_URI as string,
+      AZURE_COSMOS_CONNECTIONSTRING as string,
       options
     );
     if (connection.readyState === 1) {
